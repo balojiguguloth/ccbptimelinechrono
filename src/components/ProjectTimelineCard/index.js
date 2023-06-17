38d3283a632f3e2d1b1da2e60020591357243a43
balojiguguloth <balojiccbp4.o@gamail.com>
2023-06-17 07:56:40 +0000

    ccbptimelinechrono

---
+++ src/components/ProjectTimelineCard/index.js
@@ -0,0 +1,32 @@
+import {AiFillCalendar} from 'react-icons/ai'
+import './index.css'
+
+const ProjectTimelineCard = props => {
+  const {projectDetails} = props
+  const {
+    imageUrl,
+    projectTitle,
+    description,
+    projectUrl,
+    duration,
+  } = projectDetails
+
+  return (
+    <div className="project-card-container">
+      <img src={imageUrl} alt="project" className="project-image" />
+      <div className="project-title-duration-container">
+        <h1 className="project-title">{projectTitle}</h1>
+        <div className="duration-container">
+          <AiFillCalendar className="calendar-icon" />
+          <p className="duration">{duration}</p>
+        </div>
+      </div>
+      <p className="project-description">{description}</p>
+      <a href={projectUrl} className="visit-link">
+        Visit
+      </a>
+    </div>
+  )
+}
+
+export default ProjectTimelineCard
